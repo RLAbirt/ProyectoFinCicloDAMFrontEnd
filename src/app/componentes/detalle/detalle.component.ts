05e46d31e467d251fc05884493b191b1c3541615
Cristina Gomez <margomez@birt.eus>
2021-11-15 19:11:29 +0100

    [DEVELOP]: componente detalle. modificada la pagina de detalle-resultados para usar el componente detalle

---
+++ src/app/componentes/detalle/detalle.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.scss'],
+})
+
+// cristina: componente para mostrar el detalle de un hotel/restaurante/alojamiento/oferta
+
+export class DetalleComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
