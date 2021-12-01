1a1e7434909d50a8ed04f796aca95d436a4dd5f3
Pablo <pmato@birt.eus>
2021-12-01 17:49:17 +0100

    Prueba de funcionamiento del GPS. Está funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,7 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule, 
     IonicModule.forRoot(), 
     AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, Geolocation],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
