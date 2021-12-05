69b58f84c2186f751a8026d2de81dd2f0071d79f
Cristina Gomez <margomez@birt.eus>
2021-12-05 09:44:40 +0100

    cambio el nombre de los objetos para que coincida con las interfaces

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,10 +1,6 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Establecimiento } from '../models/InterfacesMock.interface';
-import { Hotel } from '../models/Hotel';
-import { CasaRural } from '../models/CasaRural';
-import { Restaurante } from './../models/Restaurante';
-import { Oferta } from '../models/Oferta';
+import { Hoteles, Restaurantes, CasasRurales, Ofertas} from '../interfaces/bertoninterfaces';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import 'rxjs/Rx';
@@ -22,80 +18,80 @@ export class HttpService {
 
   constructor(private http:HttpClient) { }
 
-  public getAllOfertas() :Observable<Oferta[]>{
-    return this.http.get<Oferta[]>(`${this.apiUrlMock}/ofertas`)
+  public getAllOfertas() :Observable<Ofertas[]>{
+    return this.http.get<Ofertas[]>(`${this.apiUrlMock}/ofertas`)
   }
 
-  public getAllHoteles() :Observable<Hotel[]> {
-    return this.http.get<Hotel[]>(`${this.apiUrlMock}/hoteles`);
+  public getAllHoteles() :Observable<Hoteles[]> {
+    return this.http.get<Hoteles[]>(`${this.apiUrlMock}/hoteles`);
   }
 
-  public getByGeoHoteles(lon: number, lat: number, dist: number) :Observable<Hotel[]> {
+  public getByGeoHoteles(lon: number, lat: number, dist: number) :Observable<Hoteles[]> {
     let paramsGH = new HttpParams();
     paramsGH.append('lon',lon);
     paramsGH.append('lat', lat);
     paramsGH.append('dist', dist);
 
-    return this.http.get<Hotel[]>(`${this.apiUrlMock}/hoteles/geo`,{params: paramsGH});
+    return this.http.get<Hoteles[]>(`${this.apiUrlMock}/hoteles/geo`,{params: paramsGH});
   }
 
-  public getSliceHoteles():Observable<Hotel[]> {
+  public getSliceHoteles():Observable<Hoteles[]> {
     const params = new HttpParams();
     params.set('_start', 0)
     params.set('_end', 2);
 
-    return this.http.get<Hotel[]>(`${this.apiUrlMock}/hoteles/?_start=0&_end=2`);
+    return this.http.get<Hoteles[]>(`${this.apiUrlMock}/hoteles/?_start=0&_end=2`);
   }
 
-  public getAllCasasRurales():Observable<CasaRural[]> {
-    return this.http.get<CasaRural[]>(`${this.apiUrlMock}/casasrurales`);
+  public getAllCasasRurales():Observable<CasasRurales[]> {
+    return this.http.get<CasasRurales[]>(`${this.apiUrlMock}/casasrurales`);
   }
 
-  public getByTypeCasasRurales(tipo: string):Observable<CasaRural[]> {
+  public getByTypeCasasRurales(tipo: string):Observable<CasasRurales[]> {
     let paramsR = new HttpParams();
     paramsR.set('type', tipo)
     paramsR.set('_end', 2);
 
-    return this.http.get<CasaRural[]>(`${this.apiUrlMock}/casasrurales/`,{params: paramsR});
+    return this.http.get<CasasRurales[]>(`${this.apiUrlMock}/casasrurales/`,{params: paramsR});
   }
 
-  public getSliceCasasRurales(): Observable<CasaRural[]>{
+  public getSliceCasasRurales(): Observable<CasasRurales[]>{
     let paramsSC = new HttpParams();
     paramsSC.set('_start', 0)
     paramsSC.set('_end', 2);
 
-    return this.http.get<Hotel[]>(`${this.apiUrlMock}/casasrurales/`,{params: paramsSC});
+    return this.http.get<Hoteles[]>(`${this.apiUrlMock}/casasrurales/`,{params: paramsSC});
   }
 
-  public getByGeoCasasRurales(lon: number, lat: number, dist: number, tipo: string) : Observable<CasaRural[]> {
+  public getByGeoCasasRurales(lon: number, lat: number, dist: number, tipo: string) : Observable<CasasRurales[]> {
     let paramsGC = new HttpParams();
     paramsGC.append('lon',lon);
     paramsGC.append('lat', lat);
     paramsGC.append('dist', dist);
     paramsGC.append('type', tipo);
 
-    return this.http.get<CasaRural[]>(`${this.apiUrlMock}/casasrurales/geo`,{params: paramsGC});
+    return this.http.get<CasasRurales[]>(`${this.apiUrlMock}/casasrurales/geo`,{params: paramsGC});
   }
 
-  public getAllRestaurantes(): Observable<Restaurante[]> {
-    return this.http.get<Restaurante[]>(`${this.apiUrlMock}/restaurantes`);
+  public getAllRestaurantes(): Observable<Restaurantes[]> {
+    return this.http.get<Restaurantes[]>(`${this.apiUrlMock}/restaurantes`);
   }
 
-  public getByGeoRestaurantes(lon: number, lat: number, dist: number): Observable<Restaurante[]> {
+  public getByGeoRestaurantes(lon: number, lat: number, dist: number): Observable<Restaurantes[]> {
     let paramsGR = new HttpParams();
     paramsGR.append('lon',lon);
     paramsGR.append('lat', lat);
     paramsGR.append('dist', dist);
 
-    return this.http.get<Restaurante[]>(`${this.apiUrlMock}/restaurantes/geo`,{params: paramsGR});
+    return this.http.get<Restaurantes[]>(`${this.apiUrlMock}/restaurantes/geo`,{params: paramsGR});
   }
 
-  public getSliceRestaurantes() : Observable<Restaurante[]>{
+  public getSliceRestaurantes() : Observable<Restaurantes[]>{
     let paramsSR = new HttpParams();
     paramsSR.set('_start', 0)
     paramsSR.set('_end', 2);
 
-    return this.http.get<Hotel[]>(`${this.apiUrlMock}/restaurantes`,{params: paramsSR});
+    return this.http.get<Hoteles[]>(`${this.apiUrlMock}/restaurantes`,{params: paramsSR});
   }
 
   private handleError(err: HttpErrorResponse){
