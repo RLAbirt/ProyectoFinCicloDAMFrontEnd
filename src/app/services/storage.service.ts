3190b583b1fdb6a8215f75116b23f749072ae00a
Cristina Gomez <margomez@birt.eus>
2021-12-21 12:42:13 +0100

    errores solucionado en el servicio storage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -6,6 +6,9 @@ import { Hoteles, Restaurantes, CasasRurales, Ofertas} from '../interfaces/berto
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class StorageService {
 
   private keyRestaurantes:string = "restaurantesAlmacenados"; 
@@ -13,26 +16,16 @@ export class StorageService {
   private keyCasasRurales:string = "casasRuralesAlmacenados"; 
   private keyOfertas:string = "ofertasAlmacenados"; 
 
-  private restaurantesFavoritos:Restaurantes[]; 
-  private hotelesFavoritos:Hoteles[]; 
-  private casasRuralesFavoritos:CasasRurales[]; 
-  private ofertasFavoritos:Ofertas[]; 
+  private restaurantesFavoritos:Restaurantes[]=[]; 
+  private hotelesFavoritos:Hoteles[]=[]; 
+  private casasRuralesFavoritos:CasasRurales[]=[]; 
+  private ofertasFavoritos:Ofertas[]=[]; 
   
   constructor() 
   { 
-    this.obtenerFavoritos(); 
+    
   }
 
-  // Almacena una cadena de caracteres. Para cada string se necesita un identificador (key) y su valor (value)
-  // Mediante await se indica que es un método asíncrono que devuelve una promesa
-  private async setString(key: string, value: string) {
-    await Storage.set({ key, value });
-  }
-
-  // A partir de su clave identificadora (key) obtiene el valor de una cadena de caracteres almacenada en local
-  private async getString(key: string): Promise<{ value: any }> {
-    return await Storage.get({ key });
-  }
 
   // Almacena un objeto con formato JSON en local.
   // Por cada objeto, se necesita una clave (key) y el valor del objeto (value)
@@ -47,94 +40,68 @@ export class StorageService {
     return JSON.parse(ret.value);
   }
 
-  // Dada su clave, elimina un elemento almacenado en local independientemente de su tipo
-  private async removeItem(key: string) {
-    await Storage.remove({ key });
-  }
-
   // Vacía el almacenamiento local
   private async clear() {
     await Storage.clear();
   }
 
-  probarAlmacenamiento() {
-    console.log("Aqui"); 
-    this.setObject("pablo", "prueba"); 
-
-    // Obtenemos la promesa que devuelve getObject()
-    // Debemos indicar el tipo de dato que estamos leyendo
-    let datosPromesa: Promise<String> = this.getObject("pablo");
-    // Con then() ejecutamos el código que queremos que procese los datos recibidos
-    datosPromesa.then( (data) => {
-      if(data) {
-      console.log(data);
-      }
-    })   
-
-    this.clear(); 
-  }
-
-  /*
-  <ion-list>
-    <ion-item *ngFor="let poi of poiList; let i= index" (click)="openPage(poi, i)">
-      <h2> {{ poi.name }} </h2>
-    </ion-item>
-  </ion-list>
-  https://stackoverflow.com/questions/44617412/ionic-3-get-index-of-clicked-item
-  */ 
-
+  // Carga en el array de restaurantes favoritos el contenido del storage
   private ObtenerRestaurantesFavoritos()
   {
-    // Obtenemos la promesa que devuelve getObject()
-    // Debemos indicar el tipo de dato que estamos leyendo
+    // Obtenemos la promesa que devuelve getObject(). Debemos indicar el tipo de dato que estamos leyendo
     let datosPromesa: Promise<Restaurantes[]> = this.getObject(this.keyRestaurantes);
-    // Con then() ejecutamos el código que queremos que procese los datos recibidos
+    
+    // si hay restaurantes en el storage los cargamos en nuestro array
     datosPromesa.then( (data) => {
       if(data) {
-      this.restaurantesFavoritos = data; 
+        this.restaurantesFavoritos.push(...data);
       }
-    })   
+    });
+    
   }
 
+  // Carga en el array de hoteles favoritos el contenido del storage
   private ObtenerHotelesFavoritos()
   {
-    // Obtenemos la promesa que devuelve getObject()
-    // Debemos indicar el tipo de dato que estamos leyendo
+    // Obtenemos la promesa que devuelve getObject(). Debemos indicar el tipo de dato que estamos leyendo
     let datosPromesa: Promise<Hoteles[]> = this.getObject(this.keyHoteles);
-    // Con then() ejecutamos el código que queremos que procese los datos recibidos
+    
+    // si hay hoteles en el storage los cargamos en nuestro array
     datosPromesa.then( (data) => {
-      if(data) {
-      this.hotelesFavoritos = data; 
+      if (data) {
+        this.hotelesFavoritos.push(...data); 
       }
     }) 
   }
 
+  // Carga en el array de casas rurales favoritos el contenido del storage
   private obtenerCasasRuralesFavoritos()
   {
-    // Obtenemos la promesa que devuelve getObject()
-    // Debemos indicar el tipo de dato que estamos leyendo
+    // Obtenemos la promesa que devuelve getObject(). Debemos indicar el tipo de dato que estamos leyendo
     let datosPromesa: Promise<CasasRurales[]> = this.getObject(this.keyCasasRurales);
-    // Con then() ejecutamos el código que queremos que procese los datos recibidos
+    
+    // si hay casas rurales en el storage los cargamos en nuestro array
     datosPromesa.then( (data) => {
-      if(data) {
-      this.casasRuralesFavoritos = data; 
+      if (data) {
+        this.casasRuralesFavoritos.push(...data); 
       }
     }) 
   }
 
+  // Carga en el array de ofertas favoritos el contenido del storage
   private obtenerOfertasFavoritos()
   {
-    // Obtenemos la promesa que devuelve getObject()
-    // Debemos indicar el tipo de dato que estamos leyendo
+    // Obtenemos la promesa que devuelve getObject(). Debemos indicar el tipo de dato que estamos leyendo
     let datosPromesa: Promise<Ofertas[]> = this.getObject(this.keyOfertas);
     // Con then() ejecutamos el código que queremos que procese los datos recibidos
     datosPromesa.then( (data) => {
-      if(data) {
-      this.ofertasFavoritos = data; 
+      if (data) {
+        this.ofertasFavoritos.push(...data); 
       }
     }) 
   }
 
+  // carga en los cuatro arrays de favoritos el contenido del storage
   public obtenerFavoritos()
   {
     this.ObtenerRestaurantesFavoritos(); 
@@ -165,6 +132,9 @@ export class StorageService {
 
   public aniadirRestauranteFavorito(restauranteFavorito:Restaurantes)
   {
+    // si no es favorito lo añado
+    let restauranteEncontrado = this.restaurantesFavoritos.find(x => restauranteFavorito);
+    console.log(restauranteEncontrado);
     this.restaurantesFavoritos = [...this.restaurantesFavoritos, restauranteFavorito];
     this.setObject(this.keyRestaurantes, this.restaurantesFavoritos); 
   }
