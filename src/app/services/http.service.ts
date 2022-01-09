cc0aa29816b695489970e5e5711458691804641c
Cristina Gomez <margomez@birt.eus>
2022-01-09 19:41:52 +0100

    se añaden comentarios en el servicio http

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,27 +3,38 @@ import { Injectable } from '@angular/core';
 import { Hoteles, Restaurantes, CasasRurales, Ofertas} from '../interfaces/bertoninterfaces';
 import { Observable, throwError } from 'rxjs';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
-export class HttpService {
 
-  // private apiUrl:string = 'https://10.2.106.162.labs.birt.eus:8081/';
-  // private apiUrlMock: string = '/api';
-  
+/** Servicio usado para las consultas a la API */
+export class HttpService {
 
   constructor(private http:HttpClient) { }
 
+  /**
+   * 
+   * @returns nos devuelve todas las ofertas que hay en la API
+   */
   public getAllOfertas() :Observable<Ofertas[]>{
     return this.http.get<Ofertas[]>('/api/ofertas');
   }
 
+  /**
+   * 
+   * @returns nos devuelve todos los hoteles que hay en la API
+   */
   public getAllHoteles() :Observable<Hoteles[]> {
     return this.http.get<Hoteles[]>('/api/hoteles');
   }
 
+  /**
+   * 
+   * @param lon 
+   * @param lat 
+   * @param dist 
+   * @returns nos devuelve todos los hoteles que hay a una distancia determinada teniendo en cuenta la geolocalización del usuario
+   */
   public getByGeoHoteles(lon: number, lat: number, dist: number) :Observable<Hoteles[]> {
     let paramsGH = new HttpParams();
     paramsGH.append('lon',lon);
@@ -33,10 +44,19 @@ export class HttpService {
     return this.http.get<Hoteles[]>(`/api/hoteles/geo?dist=${dist}&lat=${lat}&lon=${lon}`);
   }
 
+  /**
+   * 
+   * @returns nos devuelve todas las casas rurales de la API
+   */
   public getAllCasasRurales():Observable<CasasRurales[]> {
     return this.http.get<CasasRurales[]>('/api/casasrurales');
   }
 
+  /**
+   * 
+   * @param tipo 
+   * @returns nos devuelve todas las casas rurales
+   */
   public getByTypeCasasRurales(tipo: string):Observable<CasasRurales[]> {
     let paramsR = new HttpParams();
     paramsR.append('type', tipo)
@@ -46,6 +66,14 @@ export class HttpService {
     return this.http.get<CasasRurales[]>('/api/casasrurales/', {params: paramsR});
   }
 
+  /**
+   * 
+   * @param lon 
+   * @param lat 
+   * @param dist 
+   * @param tipo 
+   * @returns nos devuelve todas las casas rurales que hay a una distancia determinada teniendo en cuenta la geolocalización del usuario
+   */
   public getByGeoCasasRurales(lon: number, lat: number, dist: number, tipo: string) : Observable<CasasRurales[]> {
     let paramsGC = new HttpParams();
     paramsGC.append('lon',lon);
@@ -56,10 +84,21 @@ export class HttpService {
     return this.http.get<Hoteles[]>(`/api/casasrurales/geo?dist=${dist}&lat=${lat}&lon=${lon}&type=${tipo}`);
   }
 
+  /**
+   * 
+   * @returns nos devuelve todos los restaurantes de la API
+   */
   public getAllRestaurantes(): Observable<Restaurantes[]> {
     return this.http.get<Restaurantes[]>('/api/restaurantes');
   }
 
+  /**
+   * 
+   * @param lon 
+   * @param lat 
+   * @param dist 
+   * @returns nos devuelve todos los restaurantes que hay a una distancia determinada teniendo en cuenta la geolocalización del usuario
+   */
   public getByGeoRestaurantes(lon: number, lat: number, dist: number): Observable<Restaurantes[]> {
     let paramsGR = new HttpParams();
     paramsGR.append('lon',lon);
